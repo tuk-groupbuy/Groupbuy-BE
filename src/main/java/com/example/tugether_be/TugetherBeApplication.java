fe245e1283226ad87ede1ea4856d9fe67296042e
taegeon <g7193g@naver.com>
2025-07-11 13:49:06 +0900

    initial commit

---
+++ src/main/java/com/example/tugether_be/TugetherBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.tugether_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugetherBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugetherBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tugether_be/TugetherBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tugether_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TugetherBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
