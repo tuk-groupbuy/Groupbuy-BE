0dcae890e29895e97fd1dedff0e986b4a6022b6f
yunseo-1 <yunseo1229@naver.com>
2025-07-16 23:58:42 +0900

    [fix] 5. 알림 목록 조회 부분 현재인원수/최대인원수 추가

--- src/main/java/com/example/tugether_be/notification/domain/DTO/ResponseNotificationDTO.java
+++ src/main/java/com/example/tugether_be/notification/domain/DTO/ResponseNotificationDTO.java
@@ -13,4 +13,6 @@ public class ResponseNotificationDTO {
     private Boolean isRead;      // 읽음 여부
     private String createdAt;    // 알림 생성 시간 (문자열로 반환)
     private Long userId; // 참여 요청한 사용자 ID
+    private Integer currentQuantity; // 현재 인원
+    private Integer goalQuantity; // 최대 인원
 }
