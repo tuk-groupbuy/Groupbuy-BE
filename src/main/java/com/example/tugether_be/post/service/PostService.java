cfe18d01e3bd076dfd3000c6133a1af0344b2148
김람운\ramun2 <ramun22@naver.com>
2025-07-17 14:26:16 +0900

    feat: 게시물 검색 api 구현

--- src/main/java/com/example/tugether_be/post/service/PostService.java
+++ src/main/java/com/example/tugether_be/post/service/PostService.java
@@ -1,9 +1,6 @@
 package com.example.tugether_be.post.service;
 
-import com.example.tugether_be.post.bean.CreatePostBean;
-import com.example.tugether_be.post.bean.DeletePostBean;
-import com.example.tugether_be.post.bean.GetPostBean;
-import com.example.tugether_be.post.bean.UpdatePostBean;
+import com.example.tugether_be.post.bean.*;
 import com.example.tugether_be.post.domain.DTO.*;
 import com.example.tugether_be.post.service.file.FileStorageService;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +14,7 @@ import java.util.List;
 public class PostService {
 
     private final GetPostBean getPostBean;
+    private final SearchPostBean searchPostBean;
     private final CreatePostBean createPostBean;
     private final UpdatePostBean updatePostBean;
     private final DeletePostBean deletePostBean;
@@ -46,5 +44,10 @@ public class PostService {
     // 게시글 삭제
     public void deletePost(Long postId) {
         deletePostBean.exec(postId);
+    }
+
+    // 게시글 검색
+    public List<ResponsePostSummaryDTO> searchPosts(String keyword) {
+        return searchPostBean.exec(keyword);
     }
 }
