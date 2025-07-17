b9a87ca59764c29611499e18a2cefa047f062891
kwonjh0406 <kwonjh0406@gmail.com>
2025-07-17 14:26:16 +0900

    feat: user컨트롤러 테스트 로그

--- src/main/java/com/example/tugether_be/auth/controller/AuthController.java
+++ src/main/java/com/example/tugether_be/auth/controller/AuthController.java
@@ -1,7 +1,11 @@
 package com.example.tugether_be.auth.controller;
 
 import com.example.tugether_be.auth.dto.EmailDto;
+import com.example.tugether_be.auth.dto.LoginRequest;
 import com.example.tugether_be.auth.dto.SignupRequest;
+import com.example.tugether_be.auth.dto.UserIdResponse;
+import com.example.tugether_be.auth.entity.User;
+import com.example.tugether_be.auth.repository.UserRepository;
 import com.example.tugether_be.auth.service.AuthService;
 import com.example.tugether_be.auth.service.EmailVerificationService;
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +23,7 @@ public class AuthController {
 
     private final EmailVerificationService emailVerificationService;
     private final AuthService authService;
+    private final UserRepository userRepository;
 
     @PostMapping("/email/issue-code")
     public ResponseEntity<String> issueEmailCode(@RequestBody EmailDto emailDto) {
@@ -30,5 +35,11 @@ public class AuthController {
     public ResponseEntity<String> signup(@RequestBody SignupRequest request, HttpServletRequest httpRequest) {
         authService.signup(request, httpRequest);
         return ResponseEntity.ok("회원가입 성공");
+    }
+
+    @PostMapping("/login")
+    public Long login(@RequestBody LoginRequest loginRequest) {
+        User user = userRepository.findByEmail(loginRequest.getEmail()).get();
+        return user.getId();
     }
 }
---
+++ src/main/java/com/example/tugether_be/auth/dto/UserIdResponse.java
@@ -0,0 +1,12 @@
+package com.example.tugether_be.auth.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserIdResponse {
+    Long UserId;
+}
