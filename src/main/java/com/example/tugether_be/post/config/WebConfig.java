13a3bbc6e97be7a063077d721201097cc67c45f8
taegeon <g7193g@naver.com>
2025-07-17 17:53:43 +0900

    fix : fix post image

--- src/main/java/com/example/tugether_be/post/config/WebConfig.java
+++ src/main/java/com/example/tugether_be/post/config/WebConfig.java
@@ -13,6 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry
                 .addResourceHandler("/uploads/**")
-                .addResourceLocations("file:upload");
+                .addResourceLocations("file:uploads/");
     }
 }
